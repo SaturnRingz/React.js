8a55c7527e21fcc33cdfc6fcb6df42b7bbe8a3c2
emi <emijesusgon@gmail.com>
2023-03-21 12:04:48 -0300

    add register form component

---
+++ src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import RegisterForm from '../components/RegisterForm';
+
+function Register(props){
+    return(
+        <>
+        <div className="main-content">
+            
+        </div>
+        </>
+    )
+}
+export default Register;
