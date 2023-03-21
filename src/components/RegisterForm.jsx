66376a78cd35b690cc3ac253130ae397f8c48ac8
emi <emijesusgon@gmail.com>
2023-03-21 14:53:51 -0300

    make registerform body

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,7 +1,38 @@
-function RegisterForm(props){
+import LabeledInput from "./LabeledInput";
+
+function RegisterForm(){
+    return (
     <>
-    <div></div>
+    <form id="register-form">
+    <h1>Formulario de registro</h1>
+        <LabeledInput   
+        title="Nombre"                
+        type="text"
+        id="name"/>
+        <LabeledInput
+        title="Apellido"
+        type="text"
+        id="last-name"/>
+        <LabeledInput
+        title="Correo Electrónico"
+        type="email"
+        id="email"/>
+        <LabeledInput
+        title="Número de teléfono"
+        type="text"
+        id="phone"/>
+        <LabeledInput
+        title="Contraseña"
+        type="password"
+        id="pwd"/>
+        <LabeledInput
+        title="Repetir Contraseña"
+        type="password"
+        id="re-pwd"/>
+        <input type="submit" value="Registrarse" className="register-button"/>
+    </form>
     </>
+    )
 }
 
 export default RegisterForm;
