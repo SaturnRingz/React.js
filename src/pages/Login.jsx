921a011533971b724c52702fb90d4ef944072c0e
emi <emijesusgon@gmail.com>
2023-04-25 18:50:55 -0300

    add login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import LoginForm from "../components/LoginForm";
+
+function Login(props){
+return(
+<>
+<div className="login-content">
+    <div className="login-bubble">
+        <LoginForm/>
+    </div>
+</div>
+</>)
+}
+
+export default Login;
