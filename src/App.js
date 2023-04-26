fc69b43dbb51d6cc92fb7c69d96802e05ebfa3cf
emi <emijesusgon@gmail.com>
2023-04-26 20:44:03 -0300

    add navbar, import and path to future product page

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
+import ProductDetails from './pages/ProductDetails';
 import { 
   BrowserRouter as Router,
   Route,
-  Routes,
-  Navigate } from 'react-router-dom';
+  Routes,} from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
   <div className="App">
+    
     <Router>
+      <NavBar/>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
+        <Route path="/product" element={<ProductDetails/>}/>
         <Route path="/*" element={<NotFoundPage/>}/>
       </Routes>
     </Router>
