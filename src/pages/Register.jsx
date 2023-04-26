97f11509e57f2ce811febeaa252d08b5cf7acce0
emi <emijesusgon@gmail.com>
2023-04-26 20:41:05 -0300

    refactor register form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,9 +3,7 @@ import RegisterForm from '../components/RegisterForm';
 function Register(props){
     return(
     <>
-    <div className="main-content">
         <RegisterForm/>
-    </div>
     </>
     )
 }
