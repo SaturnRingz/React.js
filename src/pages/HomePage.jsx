a353a8e8886264d3d2f4fe812d4e0ec1a0ac73dc
emi <emijesusgon@gmail.com>
2023-04-26 20:42:22 -0300

    login and register moved to navbar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,7 @@ import { Link } from "react-router-dom";
 function HomePage(){
     return(
         <>
-            <button>
-                <Link to="/login">Login</Link>
-            </button>
-            <button>
-                <Link to="/register">Register</Link>
-            </button>
+            este es el home que no he terminado
         </>
     )
 }
