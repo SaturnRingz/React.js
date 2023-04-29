26e25db80759fa22d1c5411f0df068af5e9fabb1
emi <emijesusgon@gmail.com>
2023-04-29 11:08:05 -0300

    add loading screen component

---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,10 @@
+
+function LoadingScreen(){
+    return(<>
+    <div id="loading_screen">
+                <img src={require('../media/pictures/loading_ufo.png')} alt="loadingalien"></img>
+            </div>
+    </>)   
+}
+
+export default LoadingScreen
