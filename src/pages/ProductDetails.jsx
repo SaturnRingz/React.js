a3af5fc8f8afcbf7f90000e9b13a618d3bf1b260
emi <emijesusgon@gmail.com>
2023-04-30 00:30:26 -0300

    add product detail page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,4 +1,45 @@
-function ProductDetails(){
-    
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getProductById } from "../Services/productServices";
+import LoadingScreen from "../Components/LoadingScreen";
+
+function ProductDetails() {
+  
+    const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [product, setProduct] = useState({});
+  
+  
+  useEffect(() => {
+    const request = async () => {
+      try {
+        const response = await getProductById(id);
+        setProduct(response);
+        setLoading(false);
+        console.log(response)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+   request()
+
+  }, [id]);
+
+
+  if (loading) {
+    return <LoadingScreen />;
+  } else {
+    return (
+      <>
+        <div id="product-card">
+        <img src={product.pictures[0].url} alt="product-img"></img>
+        <h2>{product.title}</h2>
+        <h3>{product.price}</h3>
+        <button>Comprar</button>
+        </div>
+      </>
+    );
+  }
 }
-export default ProductDetails
+export default ProductDetails;
