804e7160f09d89d94ef380f5729a866ba7726d8b
emi <emijesusgon@gmail.com>
2023-04-30 00:32:53 -0300

    refactor loading screen component

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,10 +1,15 @@
-
-function LoadingScreen(){
-    return(<>
-    <div id="loading_screen">
-                <img src={require('../media/pictures/loading_ufo.png')} alt="loadingalien"></img>
-            </div>
-    </>)   
+function LoadingScreen() {
+  return (
+    <>
+      <div 
+        id="loading_screen">
+        <img
+          src={require("../Media/pictures/loading_ufo.png")}
+          alt="loading_alien"
+        ></img>
+      </div>
+    </>
+  );
 }
 
-export default LoadingScreen
+export default LoadingScreen;
