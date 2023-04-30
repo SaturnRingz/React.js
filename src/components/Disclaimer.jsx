28483eaa157751f988e1dd205d38d911f6670bfd
emi <emijesusgon@gmail.com>
2023-04-30 00:33:02 -0300

    add disclaimer component

---
+++ src/components/Disclaimer.jsx
@@ -0,0 +1,14 @@
+function Disclaimer() {
+  return (
+    <footer id="legal-footer">
+        <hr></hr>
+      <address>
+        Autor: <a href="emijesusgon@gmail.com">Emiliano Molina</a>.
+      </address>
+      <p>
+        <small>Código de libre distribución, segunda entrega del curso de React.JS de la UTN</small>
+      </p>
+    </footer>
+  );
+}
+export default Disclaimer;
