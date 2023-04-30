721c76d69aa69590a2067d02b683ac1dc57d4771
emi <emijesusgon@gmail.com>
2023-04-30 00:32:18 -0300

    add image acceptance to product component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom"
+
 function Product(props){
 return(<>
+    <button id="product">
+    <Link to={`/product/${props.id}`}>
+    <img src={props.imgUrl} alt={props.alt}></img>
     <h2>{props.title}</h2>
     <h3>{props.price}</h3>
+    </Link>
+    </button>
 </>)
 }
 export default Product
