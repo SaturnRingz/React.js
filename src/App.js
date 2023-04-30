94aabbd4cf3ce98bf5e8fa60d5e8cb673d87a847
emi <emijesusgon@gmail.com>
2023-04-30 00:34:01 -0300

    fixed dependencies, add Disclaimer to  every page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import HomePage from './pages/HomePage';
-import NotFoundPage from './pages/NotFoundPage';
-import ProductDetails from './pages/ProductDetails';
+import Register from './Pages/Register';
+import Login from './Pages/Login';
+import HomePage from './Pages/HomePage';
+import NotFoundPage from './Pages/NotFoundPage';
+import NavBar from './Components/NavBar';
+import ProductDetails from './Pages/ProductDetails';
+import Disclaimer from './Components/Disclaimer';
 import { 
   BrowserRouter as Router,
   Route,
   Routes,} from 'react-router-dom';
-import NavBar from './components/NavBar';
 
 function App() {
   return (
@@ -21,7 +22,9 @@ function App() {
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/*" element={<NotFoundPage/>}/>
+        <Route path="/product/:id" element={<ProductDetails/>}/>
       </Routes>
+      <Disclaimer/>
     </Router>
   </div>);
 }
