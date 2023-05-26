5136772251c7a7a6c5842aae872844f8676eca4d
Emiliano Molina <emolina@w3itsolutions.net>
2023-05-26 18:59:45 -0300

    input refactor

--- src/Components/LabeledInput.jsx
+++ src/Components/LabeledInput.jsx
@@ -1,19 +1,20 @@
-import ErrorBubble from "./ErrorBubble";
 
-
-function LabeledInput({title, type = "text", name, register, errors,
-  }){
-    return(<>
-        <label className="labeled-input">{title}
-        <div style={{height: "3px"}}></div>
-        <input  type={type}
-                name={name}
-                className="register-input"
-                {...register}
-                />
-        {errors && ErrorBubble}
-        </label>
-    </>);
+function LabeledInput({label, type, control, register, errors, rules}){
+    return(
+    <>
+      <label className="labeled-input">{label}
+      <input className="register-input"type={type}
+      {...register(control, rules)} />
+      {errors && errors[control] && (
+        <span>
+            {errors[control].type === "required" && (<p id="error-msg">Este campo es obligatorio</p>)}
+            {errors[control].type === "minLength" && (<p id="error-msg">La contraseña es muy corta</p>)}
+            {errors[control].type === "maxLength" && (<p id="error-msg">La contraseña es demasiado larga</p>)}
+        </span>
+      )}
+      </label>
+    </>
+    );
 }
 
 export default LabeledInput;
