4eb047eb0ed155473c0194eb6ac518f65de6ff81
Emiliano Molina <emolina@w3itsolutions.net>
2023-05-26 21:01:31 -0300

    refactor add product form

--- src/Pages/AddProduct.jsx
+++ src/Pages/AddProduct.jsx
@@ -1,5 +1,6 @@
 import {useForm} from 'react-hook-form';
 import { createProduct } from '../Services/productServices';
+import LabeledInput from '../Components/LabeledInput'
 function AddProduct(){
     const {register, handleSubmit, formState:{errors}} = useForm({mode: 'onSubmit'});
 
@@ -16,47 +17,34 @@ function AddProduct(){
         <div className="register-bubble">
         <form id="register-form" onSubmit={handleSubmit(onSubmit)}>
         <h1>Carga de producto</h1>
-        <label className="labeled-input">Producto
-        <div style={{height: "3px"}}></div>
-        <input  type="text"
-                name="title"
-                id="name"
-                className="register-input"
-                {...register("title", {required: true})}
-                />
-                {errors.title?.type === 'required' &&(<p id="error-msg">Este campo es obligatorio</p>)}
-        </label>
-        <label className="labeled-input">Precio
-        <div style={{height: "3px"}}></div>
-        <input  type="number"
-                name="price"
-                id="name"
-                className="register-input"
-                {...register("price", {required: true})}
-                />
-                {errors.price?.type === 'required' &&(<p id="error-msg">Este campo es obligatorio</p>)}
-                </label>
-        <label className="labeled-input">Descripción
-        <div style={{height: "3px"}}></div>
-        <input  type="text"
-                name="thumbnail"
-                id="name"
-                className="register-input"
-                {...register("details", {required: true})}
-                />
-                {errors.details?.type === 'required' &&(<p id="error-msg">Este campo es obligatorio</p>)}
-        </label>
-        <label className="labeled-input">Foto
-        <div style={{height: "3px"}}></div>
-        <input  type="text"
-                name="thumbnail"
-                id="name"
-                className="register-input"
-                {...register("thumbnail", {required: true})}
-                />
-                {errors.thumbnail?.type === 'required' &&(<p id="error-msg">Este campo es obligatorio</p>)}
-        </label>
-
+        <LabeledInput   label="Nombre del producto"
+                        type="text"
+                        control="title"
+                        register={register}
+                        rules={{required: true}}
+                        errors={errors}
+        />
+        <LabeledInput   label="Precio"
+                        type="number"
+                        control="price"
+                        register={register}
+                        rules={{required: true}}
+                        errors={errors}
+        />
+        <LabeledInput   label="Descripción"
+                        type="text"
+                        control="details"
+                        register={register}
+                        rules={{required: true}}
+                        errors={errors}
+        />
+        <LabeledInput   label="Foto"
+                        type="text"
+                        control="thumbnail"
+                        register={register}
+                        rules={{required: true}}
+                        errors={errors}
+        />
         <button className="register-button" type="submit">Guardar</button>
         </form>
         </div>
