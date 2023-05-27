754393f800cf03184e04a3dd3a4e2bde6ae59a06
Emiliano Molina <emolina@w3itsolutions.net>
2023-05-26 21:33:23 -0300

    refactor add product page

--- src/Pages/AddProduct.jsx
+++ src/Pages/AddProduct.jsx
@@ -1,54 +1,9 @@
-import {useForm} from 'react-hook-form';
-import { createProduct } from '../Services/productServices';
-import LabeledInput from '../Components/LabeledInput'
-function AddProduct(){
-    const {register, handleSubmit, formState:{errors}} = useForm({mode: 'onSubmit'});
+import AddProductForm from "../Components/AddProductForm";
 
-    const onSubmit=async (data)=>{
-        try{
-            const document = await createProduct(data);
-            console.log(document)
-        }catch(e){
-            console.log(e)
-        }
-    }
-    return(
-    <>
-        <div className="register-bubble">
-        <form id="register-form" onSubmit={handleSubmit(onSubmit)}>
-        <h1>Carga de producto</h1>
-        <LabeledInput   label="Nombre del producto"
-                        type="text"
-                        control="title"
-                        register={register}
-                        rules={{required: true}}
-                        errors={errors}
-        />
-        <LabeledInput   label="Precio"
-                        type="number"
-                        control="price"
-                        register={register}
-                        rules={{required: true}}
-                        errors={errors}
-        />
-        <LabeledInput   label="Descripción"
-                        type="text"
-                        control="details"
-                        register={register}
-                        rules={{required: true}}
-                        errors={errors}
-        />
-        <LabeledInput   label="Foto"
-                        type="text"
-                        control="thumbnail"
-                        register={register}
-                        rules={{required: true}}
-                        errors={errors}
-        />
-        <button className="register-button" type="submit">Guardar</button>
-        </form>
-        </div>
-    </>)
+function AddProduct(){
+return(<>
+    <AddProductForm/>
+</>)
 }
 
 export default AddProduct;
