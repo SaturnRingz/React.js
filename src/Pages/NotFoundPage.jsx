0db55a3e7adda862f1d564713f4ab89b3affe142
Emiliano Molina <emolina@w3itsolutions.net>
2023-05-26 21:33:42 -0300

    refactor not found page

--- src/Pages/NotFoundPage.jsx
+++ src/Pages/NotFoundPage.jsx
@@ -1,10 +1,9 @@
+import NotFound from "../Components/NotFound"
+
 function NotFoundPage(){
     return(
     <>
-        <div id="not-found-message">
-        <h2 id="notfound-title">404 Not Found</h2>
-        <p id="notfound-message">Nada por aquí...</p>
-        </div>
+        <NotFound/>
     </>)
 }
 export default NotFoundPage
