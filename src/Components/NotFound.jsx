eb214dca6754599861719b910d00d39daa4e01e2
Emiliano Molina <emolina@w3itsolutions.net>
2023-05-26 21:33:04 -0300

    add not found component

---
+++ src/Components/NotFound.jsx
@@ -0,0 +1,8 @@
+function NotFound(){
+    return(<>
+        <div id="not-found-message">
+        <h2 id="notfound-title">404 Not Found</h2>
+        <p id="notfound-message">Nada por aquí...</p>
+        </div>
+    </>)
+} export default NotFound;
