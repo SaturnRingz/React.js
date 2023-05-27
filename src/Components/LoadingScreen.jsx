7d791cfafe87592e7a8a1504f660156d9eaa6d63
Emiliano Molina <emolina@w3itsolutions.net>
2023-05-26 21:32:23 -0300

    refactor loading component

--- src/Components/LoadingScreen.jsx
+++ src/Components/LoadingScreen.jsx
@@ -1,15 +1,17 @@
-function LoadingScreen() {
-  return (
-    <>
-      <div
-        id="loading_screen">
-        <img
-          src={require("../Assets/pictures/loading_ufo.png")}
-          alt="loading_alien"
-        ></img>
-      </div>
-    </>
-  );
+function LoadingScreen({loading, children}) {
+  if(loading){
+    return (
+        <div
+          id="loading_screen">
+          <img
+            src={require("../Assets/pictures/loading_ufo.png")}
+            alt="loading_alien"
+          ></img>
+        </div>
+    );
+  } else{
+    return <>{children}</>;
+  }
 }
 
 export default LoadingScreen;
