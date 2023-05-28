03eb2e2b10f2c2b3515bb132f4fc088fcfe68db6
Emiliano Molina <emolina@w3itsolutions.net>
2023-05-28 01:35:30 -0300

    add animated button component

---
+++ src/Components/LoadButton.jsx
@@ -0,0 +1,11 @@
+function LoadButton({loading,text}){
+    if(loading){return(<>
+        <button id="load-button" className="register-button" type="submit">
+        {text}
+        <div className="custom-loader"></div>
+        </button>
+    </>)} else{ return(<><button id="load-button" className="register-button" type="submit">
+        {text}
+        </button>
+        </>)}
+} export default LoadButton;
