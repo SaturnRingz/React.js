acd85157f07e4dae6c25df9d7b0a8a0f6edc8fd2
Emiliano Molina <emolina@w3itsolutions.net>
2023-05-28 01:32:50 -0300

    add error dictionary

---
+++ src/Utils/errorMessages.js
@@ -0,0 +1,10 @@
+export const registerMessage={
+    "generic":"Ha ocurrido un error. Por favor recarga la página y vuelve a intentarlo",
+    "auth/email-already-in-use": "La dirección de correo electrónico se encuentra en uso"
+
+}
+
+export const loginMessage={
+    "generic":"Ha ocurrido un error. Por favor recarga la página y vuelve a intentarlo",
+    "auth/wrong-password": "La constraseña ingresada es incorrecta"
+}
