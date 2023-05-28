758c57c520937d86125fbeb40efe8c2a3b40cd32
Emiliano Molina <emolina@w3itsolutions.net>
2023-05-28 01:36:19 -0300

    add navbar options component

---
+++ src/Components/NavbarOptions.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+export default function NavbarOptions({isLoggedIn}) {
+    return (<ul id="navbar-options">
+        <li>
+            <Link to="/">Shop</Link>
+        </li>
+        <li>
+            <Link to="https://www.chess.com/es/play/online">Chess</Link>
+        </li>
+        <li>
+            <Link to="/*">404</Link>
+        </li>
+        {isLoggedIn && (
+            <li>
+            <div className="dropdown">
+                <button className="dropbtn">Productos▼</button>
+                <div className="dropdown-content">
+                    <Link to="/product/add">Agregar producto</Link>
+                </div>
+            </div>
+            </li>
+        )}
+    </ul>);
+}
