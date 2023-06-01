7c121e9d882aa99feef58554909e7782953d3b1c
Emiliano Molina <emolina@w3itsolutions.net>
2023-06-01 11:58:06 -0300

    refactor product component

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -3,21 +3,20 @@ import "./product.css"
 function Product({ id, title, imgUrl, alt, price, context, product_owner}) {
   return (
     <>
-      <button id="product">
-        <img src={imgUrl} alt={alt}></img>
-        <h2>{title}</h2>
-        <h3>{price}</h3>
-        <div id="product-options">
-          <Link className="product-option" to={`/product/${id}`}>
-            Detalles
-          </Link>
+      <Link id="product" to={`/product/${id}`}>
+          <img src={imgUrl} alt={alt}></img>
+          <div>
+            <h2>{title}</h2>
+            <h4>{price}</h4>
+          </div>
+          <div id="product-options">
           {context.login && context.user.uid===product_owner &&(
             <Link className="product-option" to={`/product/${id}/edit`}>
               Modificar
             </Link>
           )}
-        </div>
-      </button>
+          </div>
+      </Link>
     </>
   );
 }
